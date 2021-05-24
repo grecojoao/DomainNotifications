3d3da4b07ee2378b57fe63480f9a0a2a9e35f2aa
João Greco <junior.greco@hotmail.com>
2021-05-24 10:11:00 -0300

    Implementação do Pattern

---
+++ src/DomainNotifications/Notifiable.cs
@@ -0,0 +1,31 @@
+﻿using DomainNotifications.Contracts;
+using DomainNotifications.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DomainNotifications
+{
+    public abstract class Notifiable : INotifiable
+    {
+        public IReadOnlyCollection<Notification> Notifications { get => _notifications.ToArray(); }
+        private readonly List<Notification> _notifications = new();
+
+        public bool IsInvalid { get => Notifications.Any(); }
+        public bool IsValid { get => !IsInvalid; }
+
+        public void AddNotification(Notification notification)
+        {
+            if (notification != null)
+                _notifications.Add(notification);
+        }
+
+        public void AddNotifications(IEnumerable<Notification> notifications)
+        {
+            foreach (var notification in notifications)
+                AddNotification(notification);
+        }
+
+        public string NotificationsMessage() =>
+            string.Join("; ", Notifications.Select(x => x.Message)) + ";";
+    }
+}
