c9203542ee37b85dcc0cdcd2392ba00f7c4ccb29
João Greco <junior.greco@hotmail.com>
2021-05-26 16:08:24 -0300

    Implementado o projeto "How to use"

---
+++ samples/DomainNotifications.Examples.HowToUse/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using DomainNotifications.Entities;
+
+namespace DomainNotifications.Examples.HowToUse.Models
+{
+    class Customer : Notifiable
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public Customer() { }
+
+        public Customer(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public void Validate()
+        {
+            if (string.IsNullOrEmpty(Name))
+                AddNotification(new Notification("Name", "Invalid name"));
+            if (string.IsNullOrEmpty(Surname))
+                AddNotification(new Notification("Surname", "Invalid surname"));
+        }
+    }
+}
---
+++ samples/DomainNotifications.Examples.HowToUse/Program.cs
@@ -0,0 +1,24 @@
+﻿using DomainNotifications.Examples.HowToUse.Models;
+using System;
+
+namespace DomainNotifications.Examples.HowToUse
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            customer.Validate();
+            Console.WriteLine($"Customer validation - " +
+                $"IsInvalid: {customer.IsInvalid}, " +
+                $"Notifications: {customer.Notifications.Count}, " +
+                $"Notifications message: {customer.NotificationsMessage()}");
+
+            customer = new Customer("Name", "Surname");
+            customer.Validate();
+            Console.WriteLine($"Customer validation - " +
+                $"IsValid: {customer.IsValid}, " +
+                $"Notifications: {customer.Notifications.Count}");
+        }
+    }
+}
